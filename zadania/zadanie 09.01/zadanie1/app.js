91d49e41c1c604c791c450767195a4d5c3d73ca8
sWik-gh <sadowy_wiktor@ckziu2.edu.pl>
2025-01-09 10:04:26 +0100

    Naprawiono problem z modulami

--- zadania/zadanie 09.01/zadanie1/app.js
+++
@@ -1,7 +0,0 @@
-const calculator = require('./calculator.js')
-
-
-console.log(calculator.add(1, 3))
-console.log(calculator.subtract(1, 3))
-console.log(calculator.multiply(1, 3))
-console.log(calculator.divide(1, 3))
